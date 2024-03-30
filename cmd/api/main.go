1da3ef0b09b8a9406ba31638f9d8ce1f4c36e306
Jeremy Harrington <jerms13@gmail.com>
2024-03-29 21:06:00 -0700

    feat: init app

---
+++ cmd/api/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+	"runtime/debug"
+	"sync"
+
+	"github.com/astrojerms/build-platform-api/internal/database"
+	"github.com/astrojerms/build-platform-api/internal/smtp"
+	"github.com/astrojerms/build-platform-api/internal/version"
+
+	"github.com/lmittmann/tint"
+)
+
+func main() {
+	logger := slog.New(tint.NewHandler(os.Stdout, &tint.Options{Level: slog.LevelDebug}))
+
+	err := run(logger)
+	if err != nil {
+		trace := string(debug.Stack())
+		logger.Error(err.Error(), "trace", trace)
+		os.Exit(1)
+	}
+}
+
+type config struct {
+	baseURL   string
+	httpPort  int
+	debug     bool
+	basicAuth struct {
+		username       string
+		hashedPassword string
+	}
+	cookie struct {
+		secretKey string
+	}
+	db struct {
+		dsn         string
+		automigrate bool
+	}
+	jwt struct {
+		secretKey string
+	}
+	notifications struct {
+		email string
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		from     string
+	}
+}
+
+type application struct {
+	config config
+	db     *database.DB
+	logger *slog.Logger
+	mailer *smtp.Mailer
+	wg     sync.WaitGroup
+}
+
+func run(logger *slog.Logger) error {
+	var cfg config
+
+	flag.StringVar(&cfg.baseURL, "base-url", "http://localhost:4444", "base URL for the application")
+	flag.IntVar(&cfg.httpPort, "http-port", 4444, "port to listen on for HTTP requests")
+	flag.StringVar(&cfg.basicAuth.username, "basic-auth-username", "admin", "basic auth username")
+	flag.StringVar(&cfg.basicAuth.hashedPassword, "basic-auth-hashed-password", "$2a$10$jRb2qniNcoCyQM23T59RfeEQUbgdAXfR6S0scynmKfJa5Gj3arGJa", "basic auth password hashed with bcrpyt")
+	flag.StringVar(&cfg.cookie.secretKey, "cookie-secret-key", "uyfzmelmrkgsa376z53wudexfnaym6bm", "secret key for cookie authentication/encryption")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres:password@127.0.0.1:5432/postgres", "postgreSQL DSN")
+	flag.BoolVar(&cfg.db.automigrate, "db-automigrate", true, "run migrations on startup")
+	flag.StringVar(&cfg.jwt.secretKey, "jwt-secret-key", "5quzvhgf3kefilsw3dif6deswdcirplg", "secret key for JWT authentication")
+	flag.StringVar(&cfg.notifications.email, "notifications-email", "", "contact email address for error notifications")
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "example.smtp.host", "smtp host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 25, "smtp port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "example_username", "smtp username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "pa55word", "smtp password")
+	flag.StringVar(&cfg.smtp.from, "smtp-from", "Example Name <no-reply@example.org>", "smtp sender")
+	flag.BoolVar(&cfg.debug, "debug", false, "enable debug mode")
+
+	showVersion := flag.Bool("version", false, "display version and exit")
+
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("version: %s\n", version.Get())
+		return nil
+	}
+
+	db, err := database.New(cfg.db.dsn, cfg.db.automigrate)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	mailer, err := smtp.NewMailer(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.from)
+	if err != nil {
+		return err
+	}
+
+	app := &application{
+		config: cfg,
+		db:     db,
+		logger: logger,
+		mailer: mailer,
+	}
+
+	return app.serveHTTP()
+}
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/astrojerms/build-platform-api
+
+go 1.22.0
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.17.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
+	github.com/lmittmann/tint v1.0.4
+	github.com/pascaldekloe/jwt v1.12.0
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
+	github.com/wneessen/go-mail v0.4.1
+	golang.org/x/crypto v0.19.0
+	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a
+	golang.org/x/text v0.14.0
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
